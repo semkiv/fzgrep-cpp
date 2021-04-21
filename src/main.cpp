3d7808b84660d0bae4ecdaca543adb1fa426128a
Andrii Semkiv <semkiv@gmail.com>
2021-04-21 22:29:05 +0300

    Little preparations
    Added CMake configuration.
    Added CMake specific files to gitignore.
    Added clang-tidy settings file.
    Added clang-format settings file.
    Added a hello world application.

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib>
+#include <iostream>
+
+auto main([[maybe_unused]] int const argc, [[maybe_unused]] char const * const argv[]) -> int {
+    std::cout << "Hello, world!" << std::endl;
+
+    return EXIT_SUCCESS;
+}
