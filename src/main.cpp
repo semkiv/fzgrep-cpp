2a864cd2115891092a009f91afea86b71b650ec7
Andrii Semkiv <semkiv@gmail.com>
2021-04-22 19:17:45 +0300

    Little preparations
    Modernized CMake configuration.
    Added GoogleTest.
    Hello world program now uses some `jthread`s from C++20.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <cstdlib>
 #include <iostream>
+#include <thread>
 
 auto main([[maybe_unused]] int const argc, [[maybe_unused]] char const * const argv[]) -> int {
-    std::cout << "Hello, world!" << std::endl;
+    auto greeter = std::jthread{[] { std::cout << "Hello, world!" << std::endl; }};
 
     return EXIT_SUCCESS;
 }
